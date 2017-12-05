13db0dfbfb53c2a6b7b2442ebb521bc1c480c4fa
classicmayo <classicmayo@users.noreply.github.com>
2017-12-05 11:07:35 -0500

    add gitignore file

--- test.cpp
+++ test.cpp
@@ -27,8 +27,9 @@ int main(int, char**)
 {
   
   std::string hex_str = "000000000000000000ad6e90c0790e83760a9d13728c23474352a2c8c7a6e0eb";
+  std::string hex_str2 = "026765a1c8235d4ac23d2582cda3b9f5c062f805540320173eb9e9148c0dc518704b42e4b11ca131b443c2b02a07ec0b45407f1b125027e3e68b86ace692445800000001";
 
-  std::cout << picosha2::hash256_hex_string(hexToString(hex_str)) << std::endl;
+  std::cout << picosha2::hash256_hex_string(hexToString(hex_str2)) << std::endl;
 
   return 0;
 }
